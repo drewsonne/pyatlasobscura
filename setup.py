a6dd65ffec6dee475e14d62e14a43a446b334364
Drew J. Sonne <drew.sonne@gmail.com>
2018-11-18 13:09:14 +0000

    Set minimum python version

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='pyatlasobscura',
     version='0.1.0',
     packages=find_packages(),
+    python_requires='>3.5.0',
     url='https://github.com/drewsonne/pyatlasobscura',
     license='LGPLv3',
     author='Drew J. Sonne',
